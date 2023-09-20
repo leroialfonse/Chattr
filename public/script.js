b8721a3132d0b04a1f0795a749522c31770c8909
Brandon White <brandonwhitedev@gmail.com>
2023-09-20 03:49:25 -0400

    Tried adding rooms again, but still getting socket.to(room).broadcast.emit('user-connected', name)                       cannot read properties of undefined emit at server 195.

--- public/script.js
+++ public/script.js
@@ -1,131 +1,188 @@
-// // Tell the app what to use when it sees "socket".
-const socket = io('http://localhost:4040');
+// // // Tell the app what to use when it sees "socket".
+// // const socket = io('http://localhost:4040');
 
-// Create variables to affect the form and input client side.
-// The chat messages
-const messages = document.getElementById('messages')
-// The input where users interract with the app...
-const form = document.getElementById('form')
-// ...and the input where they type messages.
-const input = document.getElementById('input')
-// Where the 'user is typing' message lives.
-let typing = document.getElementById('typing')
-// Address the room conatiner.
+// // Create variables to affect the form and input client side.
+// // The chat messages
+// const messages = document.getElementById('messages')
+// // The input where users interract with the app...
+// const form = document.getElementById('form')
+// // ...and the input where they type messages.
+// const input = document.getElementById('input')
+// // Where the 'user is typing' message lives.
+// let typingBlock = document.getElementById('typingBlock')
+// // Address the room conatiner.
+// const roomContainer = document.getElementById('room-container')
+
+
+
+// // Check to see if the user has already entered a name.
+// if (form != null) {
+//     // Supply the user's name.
+//     const name = prompt('what is your name?')
+
+//     // Alert that the user has joined
+//     appendMessage('You joined the chat!')
+//     console.log(roomName)
+//     socket.emit('new-user', roomName, name)
+
+//     // An event listener to respond the teh send button being clicked to send a message.
+//     form.addEventListener('submit', e => {
+//         // Stops the input button from inputting immediately on page load
+//         e.preventDefault()
+//         // Pull the message from the input
+//         const message = input.value
+//         // Put that value into the chat.
+//         appendMessage(`You: ${message}`)
+
+//         // Show that message to all connected users
+//         // This displays the message in my screen for now...
+//         socket.emit("send-chat-message", roomName, message)
+//         console.log(name, message)
+
+//         //  clear out the input after each submit fires.
+//         input.value = ''
+//         window.scrollTo(0, 9999)
+//     })
+// }
+
+
+
+// // Alert when a room is connected, will take the name of the room as a param.
+// socket.on('room-created', room => {
+
+//     // will create a room using this format. 
+//     // <div> <%= room %> </div>
+//     // <a href="/<%= room %>">Join</a>
+
+//     const roomElement = document.createElement('div')
+
+//     // Trying to welcome to the specific room.
+//     // roomElement.innerText = `Welcome to the ${room} room!`
+
+//     // Just display the room name 
+//     roomElement.innerText = room
+
+//     //  A link to the created room.
+//     const roomLink = document.createElement('a')
+//     //  Sends a user to that room.
+//     roomLink.href = `/${room}`
+//     roomLink.innerText = 'Join'
+
+//     // Add this code to the page
+//     roomContainer.append(roomElement)
+//     roomContainer.append(roomLink)
+// })
+
+
+
+
+// // When a chat message event fires, client side captures, 
+// socket.on('chat-message', data => {
+//     // ... and creates a li element in the ul that displays that message..
+//     appendMessage(`${data.name}: ${data.message}`)
+//     // and console.logs it 
+//     console.log(`${data.name}:  ${data.message}`)
+//     // Clear the typing element when a user finishes typing.
+//     typing.innerHTML = ''
+//     // continue scrolling the screen to keep the chat in focus. Still considering that part. 
+//     // window.scrollTo(0, document.body.scrollHeight);
+// })
+
+// // // Alert when someone is typing
+// // socket.on('typing', function (data) {
+// //     // Display the user typing message in the "typing " element.
+// //     typing.innerHTML = '<p>' + data + ' is typing...</p>'
+
+// // })
+
+
+// // Alert the chat when a user connects, and who they are 
+// socket.on('user-connected', name => {
+//     console.log(`${name} connected!`)
+//     appendMessage(`${name} connected!`)
+// })
+
+// // And when they disconnect.
+// socket.on('user-disconnected', name => {
+//     appendMessage(`...${name} disconnected.`)
+// })
+
+
+
+
+// // Will listen to the form for keypress, and pop the user is typing message
+// // input.addEventListener('keypress', function (name) {
+// //     socket.emit('typing', name)
+// // })
+
+// // create the message bubbles that contain messages. 
+// function appendMessage(message) {
+//     const messageElement = document.createElement('div')
+//     messageElement.innerText = message
+//     messages.append(messageElement)
+// }
+
+
+const socket = io('http://localhost:8500')
+
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+const messageContainer = document.getElementById('message-container')
 const roomContainer = document.getElementById('room-container')
 
-
-
-// Check to see if the user has already entered a name.
-if (form != null) {
-    // Supply the user's name.
-    const name = prompt('what is your name?')
-
-    // Alert that the user has joined
+if (messageForm != null) {
+    const name = prompt('What is your name??')
     appendMessage('You joined the chat!')
-    console.log(roomName)
+    // Sends this message to the server, and emits whenever a new user is signed up.
     socket.emit('new-user', roomName, name)
 
-    // An event listener to respond the teh send button being clicked to send a message.
-    form.addEventListener('submit', e => {
-        // Stops the input button from inputting immediately on page load
+    messageForm.addEventListener('submit', e => {
         e.preventDefault()
-        // Pull the message from the input
-        const message = input.value
-        // Put that value into the chat.
+        const message = messageInput.value
+        // call the append message event inside of the event to display the message on screen.
+
         appendMessage(`You: ${message}`)
 
-        // Show that message to all connected users
-        // This displays the message in my screen for now...
-        socket.broadcast.emit("send-chat-message", roomName,
-            message
+        socket.emit('send-chat-message', roomName, message)
 
-            // This doesn't yet display anything.
-            // socket.emit("send-chat-message", roomName,
-            // message
-        )
-        console.log(name, message)
-
-        //  clear out the input after each submit fires.
-        input.value = ''
-        // window.scrollTo(0, 9999)
+        messageInput.value = ''
     })
-
-
 }
 
-
-
-// Alert when a room is connected, will take the name of the room as a param.
 socket.on('room-created', room => {
-
-    // will create a room using this format. 
-    // <div> <%= room %> </div>
-    // <a href="/<%= room %>">Join</a>
-
-    // Some imperative JS to create a room, call out that it's been created, and add notes
     const roomElement = document.createElement('div')
-
-    // Trying to welcome to the specific room.
-    // roomElement.innerText = `Welcome to the ${room} room!`
-
-    // Just display the room name 
     roomElement.innerText = room
-
-    //  A link to the created room.
     const roomLink = document.createElement('a')
-    //  Sends a user to that room.
     roomLink.href = `/${room}`
-    roomLink.innerText = 'Join'
-
-    // Add this code to the page
+    roomLink.innerText = 'join'
     roomContainer.append(roomElement)
     roomContainer.append(roomLink)
 })
 
-
-
-
-// When a chat message event fires, client side captures, 
 socket.on('chat-message', data => {
-    // ... and creates a li element in the ul that displays that message..
+    // Appends our message element with the user's name and their message.
     appendMessage(`${data.name}: ${data.message}`)
-    // and console.logs it 
-    console.log(`${data.name}:  ${data.message}`)
-    // Clear the typing element when a user finishes typing.
-    typing.innerHTML = ''
-    // continue scrolling the screen to keep the chat in focus. 
-    window.scrollTo(0, document.body.scrollHeight);
 })
 
 
-
-// Alert the chat when a user connects, and who they are 
+// Alerts a user has connected by their name in the chat window.
 socket.on('user-connected', name => {
-    console.log(`${name} connected!`)
-    appendMessage(`${name} connected!`)
+    appendMessage(`${name} has connected! `)
 })
 
-// And when they disconnect.
+// Alerts when a user disconnects.
 socket.on('user-disconnected', name => {
-    appendMessage(`...${name} disconnected.`)
-})
-
-// Alert when someone is typing
-socket.on('typing', function (name) {
-    // Display the user typing message in the "typing " element.
-    typing.innerHTML = `<p>${name} is typing</p>`
+    appendMessage(`....${name} disconnected.`)
 
 })
 
 
-// Will listen to the form for keypress, and pop the user is typing message
-input.addEventListener('keypress', function (name) {
-    socket.emit('typing', name)
-})
 
-// create the message bubbles that contain messages. 
+
+
 function appendMessage(message) {
-    const messageElement = document.createElement('li')
+
+    const messageElement = document.createElement('div')
     messageElement.innerText = message
-    messages.append(messageElement)
+    messageContainer.append(message)
 }
--- server.js
+++ server.js
@@ -1,125 +1,216 @@
-// Go get express and use 'app' to handle functions...
-const express = require("express");
-const app = express();
+// // Go get express and use 'app' to handle functions...
+// const express = require("express");
+// const app = express();
 
-// Set up the server to use http.
-const server = require('http').Server(app)
+// // Set up the server to use http.
+// const server = require('http').Server(app)
 
-// Setting up socket.
-const io = require("socket.io")(server)
+// // Setting up socket.
+// const io = require("socket.io")(server)
 
-// Define the port.
-const PORT = process.env.PORT || 4040
+// // Define the port.
+// const PORT = process.env.PORT || 4040
 
-// Use http for express to work with server.
-const http = require('http')
+// // Use http for express to work with server.
+// const http = require('http')
 
-// Define a view engine and set it to use EJS
-app.set('views', './views')
-app.set('view engine', 'ejs')
-
-
-// For the static files.
-app.use(express.static('public'))
+// // Define a view engine and set it to use EJS
+// app.set('views', './views')
+// app.set('view engine', 'ejs')
 
 
-// Be able to accept url params
-app.use(express.urlencoded({ extended: true }))
+// // For the static files.
+// app.use(express.static('public'))
 
 
-// Define the rooms object so that you can call/use different rooms.
-const rooms = {}
-
-// Route to the index page, and pass the render engine all of the rooms, too.
-app.get('/', (req, res) => {
-    res.render('index', { rooms: rooms })
-})
-
-// // Get a route for the rooms.
-app.post('/room', (req, res) => {
-    // Check that a room is not being assigned the same name as another room. If it is, redirect to the index.
-    if (rooms[req.body.room] != null) {
-        return res.redirect('/')
-    }
-    // Which room are we looking for?
-    rooms[req.body.room] = { users: {} }
-    // Direct the user to the new room after selection.
-    res.redirect(req.body.room)
-    // Send a message that a room was created...
-
-    io.emit('New room created!', req.body.room)
-})
+// // Be able to accept url params
+// app.use(express.urlencoded({ extended: true }))
 
 
+// // Define the rooms object so that you can call/use different rooms.
+// const rooms = {}
+
+// // Route to the index page, and pass the render engine all of the rooms, too.
+// app.get('/', (req, res) => {
+//     res.render('index', { rooms: rooms })
+// })
+
+// // // Get a route for the rooms.
+// app.post('/room', (req, res) => {
+//     // Check that a room is not being assigned the same name as another room. If it is, redirect to the index.
+//     if (rooms[req.body.room] != null) {
+//         return res.redirect('/')
+//     }
+//     // Which room are we looking for?
+//     rooms[req.body.room] = { users: {} }
+//     // Direct the user to the new room after selection.
+//     res.redirect(req.body.room)
+//     // Send a message that a room was created...
+
+//     io.emit('New room created!', req.body.room)
+// })
 
 
-// Get a differnt room, with 'room' as a param. SO, a user can select a room to go into.
-app.get('/:room', (req, res) => {
-    // redirect a user back to home if a room name supplied doesn't exist.
-    if (rooms[req.params.room] == null) {
-        return res.redirect('/')
-    }
 
-    res.render('room', { roomName: req.params.room })
-})
 
-// Set up an object to store user information. 
-// Won't need this object once I have rooms set up, because I only want to use it inside of rooms.
+// // Get a differnt room, with 'room' as a param. SO, a user can select a room to go into.
+// app.get('/:room', (req, res) => {
+//     // redirect a user back to home if a room name supplied doesn't exist.
+//     if (rooms[req.params.room] == null) {
+//         return res.redirect('/')
+//     }
+
+//     res.render('room', { roomName: req.params.room })
+// })
+
+// // Set up an object to store user information. 
+// // Won't need this object once I have rooms set up, because I only want to use it inside of rooms.
 // const users = {}
 
 
-// On init of a socket, notify the connetion in the console.
+// // On init of a socket, notify the connetion in the console.
+// io.on('connection', socket => {
+
+
+//     socket.on('new-user', (room, name) => {
+//         // Connects the socket to the room created 
+//         socket.join(room)
+
+//         // Sends users to the specific room.
+//         // Notify of connection to socket and socket id as name
+//         rooms[room].users[socket.id] = name
+//         socket.to(room).emit('user connected', name)
+//         console.log(`${name} connected!`)
+//     })
+
+
+//     // Original.
+//     // socket.on('send chat message', (room, message) => {
+//     // Send everyone but the sending socket the message, ut only to users in this current room.
+//     //     socket.to(room).broadcast('chat message', { message: message, name: rooms[room].users[socket.id] })
+//     // })
+
+//     // Trying
+
+//     socket.on('send chat message', (room, message) => {
+//         socket.to(room).emit('chat message', { message: message, name: rooms[room].users[socket.id] });
+//     })
+
+
+//     // A notification of a user disconnect.
+//     socket.on('disconnect', () => {
+//         // tell all users connected to the socket that a certain user has disconnected.
+//         getUserRooms(socket).forEach(room => {
+//             socket.to(room).emit('user disconnected', rooms[room].users[socket.id])
+//             console.log(`...${users[socket.id]} disconnected.`)
+//             // Remove a user
+//             delete rooms[room].users[socket.id]
+//         })
+
+//     })
+// })
+
+// // Someone is typing
+// // socket.on(
+// //     'typing', function (data, name) {
+// //         socket.broadcast.emit('typing', data)
+// //     })
+
+// function getUserRooms(socket) {
+//     return Object.entries(rooms).reduce((names, [name, room]) => {
+//         if (room.users[socket.id] != null) names.push(name)
+//         return names
+//     }, [])
+// }
+// // Tell the server that Socekt will connect to which port to listen for requests on
+// server.listen(PORT, () => {
+//     console.log(`Listening on port ${PORT}`)
+// })
+
+
+
+
+
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const cors = require('cors')
+const PORT = 8500
+
+
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+// A variable that is an empty object that stores the rooms.
+const rooms = {}
+
+
+app.get('/', (req, res) => {
+    // want to render the indexpage, and pass in all the chat rooms available/existing. This will pass to the view.
+    res.render('index', { rooms: rooms })
+})
+
+app.post('/room', (req, res) => {
+    if (rooms[req.body.room] != null) {
+        return res.redirect('/')
+    }
+
+    // respond to the room form. 
+    rooms[req.body.room] = { users: {} }
+    res.redirect(req.body.room)
+
+    // Send a message that the new room was created. 
+    io.emit('room-created', req.body.room)
+})
+
+// Routing into a specific room. 
+app.get('/:room', (req, res) => {
+    if (rooms[req.params.room] == null) {
+        return res.redirect('/')
+    }
+    // renders the room based on the req param of the name of the room the user selects.
+    res.render('room', { roomName: req.params.room })
+})
+
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
+
+
+
+
+
 io.on('connection', socket => {
-
-
-    socket.on('new user', (room, name) => {
+    // catch the new user emit from the client side.
+    socket.on('new-user', (room, name) => {
         socket.join(room)
-
-        // Sends users to the specific room.
-        // Notify of connection to socket and socket id as name
-        rooms[room].users[socket.id] = name || "User"
-        socket.to(`${room}`).emit('user connected', users[socket.id])
-        console.log(`${name} connected!`)
+        //  Want the key of the users object to be the id of the socket
+        rooms[room].users[socket.id] = name
+        // Emit a user connected message.
+        socket.to(room).broadcast.emit('user-connected', name)
     })
 
-
-    // Original.
-    socket.on('send chat message', (room, message) => {
-        // Send everyone but the sending socket the message, ut only to users in this current room.
-        socket.to(room).emit('chat message', { message: message, name: rooms[room].users[socket.id] })
+    socket.on('send-chat-message', (room, message) => {
+        // Emit the message itself, and the user, by broadcasting the message and name as an object. The first element will be the message, the second will be the user's name.
+        socket.to(room).broadcast.emit('chat-message', { message: message, name: rooms[room].users[socket.id] })
     })
 
-    // Trying
-
-    // socket.on('send chat message', (room, message) => {
-    //     if (rooms[room] && rooms[room].users && rooms[room].users[socket.id]) {
-    //         socket.to(room).emit('chat message', { message: message, name: rooms[room].users[socket.id] });
-    //     } else {
-    //         // Handle the case where the room or user is not found.
-    //         console.error('Error: Room or user not found.');
-    //     }
-    // });
-
-
-
-    // A notification of a user disconnect.
     socket.on('disconnect', () => {
-        // tell all users connected to the socket that a certain user has disconnected.
+
         getUserRooms(socket).forEach(room => {
-            socket.broadcast.emit('user disconnected', rooms[room].users[socket.id])
-            console.log(`...${users[socket.id]} disconnected.`)
-            // Remove a user
+            socket.broadcast.emit('user-disconnected', rooms[room].users[socket.id])
             delete rooms[room].users[socket.id]
         })
 
     })
 
-    // Someone is typing
-    socket.on(
-        'typing', function (data, name) {
-            socket.broadcast.emit('typing', data)
-        })
-});
+})
+
 
 function getUserRooms(socket) {
     return Object.entries(rooms).reduce((names, [name, room]) => {
@@ -127,7 +218,3 @@ function getUserRooms(socket) {
         return names
     }, [])
 }
-// Tell the server that Socekt will connect to which port to listen for requests on
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-})
